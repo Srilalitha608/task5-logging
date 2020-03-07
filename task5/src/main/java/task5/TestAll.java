21c264171f19ab7398f003a23643fd60ee21d773
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-03-07 20:10:13 +0530

    task5-logging

---
+++ task5/src/main/java/task5/TestAll.java
@@ -0,0 +1,11 @@
+package task5;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ House_ConstructionTest.class, SI_CITest.class })
+
+public class TestAll {
+
+}
